89bb1fabc29c5a1500c39d118e255cbf297286c8
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-25 21:55:38 -0800

    Test against transcritical normal form

--- src/examples/examples.jl
+++ src/examples/examples.jl
@@ -1,10 +1,12 @@
 module Examples
 
 include("pitchfork.jl")
+include("transcritical.jl")
 include("calcium.jl")
 
 const PROBLEMS = [
     Pitchfork.prob,
+    Transcritical.prob,
     Calcium.prob,
 ]
 end  # module
---
+++ src/examples/transcritical.jl
@@ -0,0 +1,23 @@
+"""
+The normal form of transcritical bifurcations.
+
+https://en.wikipedia.org/wiki/Transcritical_bifurcation
+"""
+module Transcritical
+
+using DiffEqBase: ODEProblem
+using Setfield: @lens
+
+using ...Bifurcations: FixedPointBifurcationProblem
+
+f(u, p, t) = p * u - u^2
+
+u0 = 0.0
+tspan = (0.0, 1.0)
+p = -1.0
+ode = ODEProblem(f, u0, tspan, p)
+
+param_axis = @lens _
+prob = FixedPointBifurcationProblem(ode, param_axis, (-1.0, 1.0))
+
+end  # module
