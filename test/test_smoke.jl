36d2446a9f7f3f5e7db79e2705c2d4299c1720e6
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-19 14:36:04 -0700

    Render plots in test_smoke.jl

--- test/test_smoke.jl
+++ test/test_smoke.jl
@@ -1,14 +1,20 @@
 module TestSmoke
+using Compat
 using Base.Test
 using Plots
 using Bifurcations
 using Bifurcations.Examples: PROBLEMS
 include("utils.jl")
 
+function nullshow(plt)
+    show(devnull, MIME("image/png"), plt)
+end
+
 for prob in PROBLEMS
     @test_nothrow begin
         sol = solve(prob)
-        plot(sol)
+        plt = plot(sol)
+        nullshow(plt)
     end
 end
 
