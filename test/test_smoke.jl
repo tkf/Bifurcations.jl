769d8b2e53f300a4b1e73675c49bf2c87d068ebb
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-21 19:20:13 -0700

    Suppress/test warnings in test_smoke.jl

--- test/test_smoke.jl
+++ test/test_smoke.jl
@@ -5,6 +5,7 @@ import GR  # Workaround Plots.jl world age problem
 using Plots
 
 using Bifurcations
+using Bifurcations: plot  # TODO: stop doing this
 using Bifurcations.Codim1: resolved_points, SpecialPoint
 using Bifurcations.Examples: PROBLEMS
 include("utils.jl")
@@ -37,12 +38,24 @@ end
         @test_nothrow nullshow(plot(sol; include_points=true))
 
         @test_nothrow nullshow(plot(solver))
-        @test_nothrow nullshow(plot(solver; include_points=true))
+        @test_nothrow nullshow(plot(solver; include_points=false))
 
         for p in points
             @test_nothrow nullshow(plot(p))
         end
     end
+end
+
+@testset "warn" begin
+    plot = Plots.plot
+    solver = init(Bifurcations.Examples.Calcium.prob)
+    solve!(solver)
+    sol = solver.sol
+
+    msg = "include_points = true"
+    @test_warn msg nullshow(plot(sol.sweeps[1]; include_points=true))
+    @test_warn msg nullshow(plot(sol; include_points=true))
+    @test_warn msg nullshow(plot(solver))
 end
 
 end  # module
