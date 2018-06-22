0e3d6c9257abc21db20d2e424e595587793d06f4
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-22 16:23:34 -0700

    Move codim1/interface.jl -> codim1/tools.jl

--- src/codim1/codim1.jl
+++ src/codim1/codim1.jl
@@ -2,7 +2,7 @@ module Codim1
 using Compat
 include("base.jl")
 include("analysis.jl")
-include("interface.jl")
+include("tools.jl")
 include("resolve_point.jl")
 include("display.jl")
 include("diagnosis.jl")
