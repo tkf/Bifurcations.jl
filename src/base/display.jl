32991616189121fab279228ec3442a0d26c58cc2
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-22 20:48:35 -0700

    Move Codim1* show methods to base/display.jl

--- src/base/display.jl
+++ src/base/display.jl
@@ -1,13 +1,25 @@
+aliasof(x) = nameof(typeof(x))
+
 tkindstr(x) = tkindstr(timekind(x))
 tkindstr(::Discrete) = "Discrete"
 tkindstr(::Continuous) = "Continuous"
 
+tkindstr(sol::BifurcationSolution) =
+    isempty(sol.sweeps) ? "?" : tkindstr(sol.sweeps[1])  # FIXME
+tkindstr(solver::BifurcationSolver) = tkindstr(solver.sol)
+
 function print_header(io::IO, point::Union{SpecialPoint,
                                            SpecialPointInterval})
-    print(io, nameof(typeof(point)), " <",
+    print(io, aliasof(point), " <",
           tkindstr(point), " ",
           point.point_type,
           ">")
+end
+
+function print_header(io::IO, ctx::Union{BifurcationSweep,
+                                         BifurcationSolution,
+                                         BifurcationSolver})
+    print(io, aliasof(ctx), " <", tkindstr(ctx), ">")
 end
 
 set_if_not(io, key, val) = haskey(io, key) ? io : IOContext(io, key => val)
@@ -30,4 +42,51 @@ function Base.show(io::IO, point::SpecialPointInterval)
         println(io, "  u0 = ", point.u0)
         println(io, "  u1 = ", point.u1)
     end
+end
+
+function Base.show(io::IO, sweep::BifurcationSweep)
+    super = as(sweep, ContinuationSweep)
+    print_header(io, sweep)
+
+    n_all = length(sweep)
+    n_sb = length(super.simple_bifurcation)
+    n_sp = length(sweep.special_points)
+    if get(io, :compact, false)
+        println(io, " ", n_sb + n_sp, "/", n_all, " special/points")
+    else
+        println(io)
+        println(io, "# points             : ", n_all)
+        println(io, "# simple bifurcations: ", n_sb)
+        println(io, "# special points     : ", n_sp)
+    end
+end
+
+function show_solution_info(io::IO, sol::BifurcationSolution)
+    super = as(sol, ContinuationSolution)
+    if isempty(sol.sweeps)
+        println(io, " no sweeps")
+        return
+    end
+    n_all = sum(length, super.sweeps)
+    n_sb = sum(length(s.simple_bifurcation) for s in super.sweeps)
+    n_sp = sum(length(s.special_points) for s in sol.sweeps)
+    if get(io, :compact, false)
+        println(io, " ", n_sb + n_sp, "/", n_all, " special/points")
+    else
+        println(io)
+        println(io, "# sweeps             : ", length(sol.sweeps))
+        println(io, "# points             : ", n_all)
+        println(io, "# simple bifurcations: ", n_sb)
+        println(io, "# special points     : ", n_sp)
+    end
+end
+
+function Base.show(io::IO, sol::BifurcationSolution)
+    print_header(io, sol)
+    show_solution_info(io, sol)
+end
+
+function Base.show(io::IO, solver::BifurcationSolver)
+    print_header(io, solver)
+    show_solution_info(io, solver.sol)
 end
--- src/codim1/display.jl
+++ src/codim1/display.jl
@@ -1,52 +1,5 @@
-using ..BifurcationsBase: tkindstr
+import ..BifurcationsBase
 
-
-function Base.show(io::IO, sweep::Codim1Sweep)
-    super = as(sweep, ContinuationSweep)
-    print(io, "Codim1Sweep <", tkindstr(sweep), ">")
-
-    n_all = length(sweep)
-    n_sb = length(super.simple_bifurcation)
-    n_sp = length(sweep.special_points)
-    if get(io, :compact, false)
-        println(io, " ", n_sb + n_sp, "/", n_all, " special/points")
-    else
-        println(io)
-        println(io, "# points             : ", n_all)
-        println(io, "# simple bifurcations: ", n_sb)
-        println(io, "# special points     : ", n_sp)
-    end
-end
-
-function show_solution_info(io::IO, sol::Codim1Solution)
-    super = as(sol, ContinuationSolution)
-    if isempty(sol.sweeps)
-        println(io, " no sweeps")
-        return
-    end
-    n_all = sum(length, super.sweeps)
-    n_sb = sum(length(s.simple_bifurcation) for s in super.sweeps)
-    n_sp = sum(length(s.special_points) for s in sol.sweeps)
-    if get(io, :compact, false)
-        println(io, " ", n_sb + n_sp, "/", n_all, " special/points")
-    else
-        println(io)
-        println(io, "# sweeps             : ", length(sol.sweeps))
-        println(io, "# points             : ", n_all)
-        println(io, "# simple bifurcations: ", n_sb)
-        println(io, "# special points     : ", n_sp)
-    end
-end
-
-function Base.show(io::IO, sol::Codim1Solution)
-    tkind = isempty(sol.sweeps) ? "?" : tkindstr(sol.sweeps[1]) # FIXME
-    print(io, "Codim1Solution <", tkind, ">")
-    show_solution_info(io, sol)
-end
-
-function Base.show(io::IO, solver::Codim1Solver)
-    sol = solver.sol
-    tkind = isempty(sol.sweeps) ? "?" : tkindstr(sol.sweeps[1]) # FIXME
-    print(io, "Codim1Solver <", tkind, ">")
-    show_solution_info(io, sol)
-end
+BifurcationsBase.aliasof(::Codim1Sweep) = "Codim1Sweep"
+BifurcationsBase.aliasof(::Codim1Solution) = "Codim1Solution"
+BifurcationsBase.aliasof(::Codim1Solver) = "Codim1Solver"
