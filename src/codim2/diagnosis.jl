e3f110cdf746182ad57386566dfeef89f064c7fa
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-29 20:28:01 -0700

    Ignore BackReferencingAS problems in find_errors

--- src/codim2/diagnosis.jl
+++ src/codim2/diagnosis.jl
@@ -1,6 +1,12 @@
 import ..Continuations: find_errors
 
 function find_errors(solver::Codim2Solver)
+    if augsys(solver.prob) isa BackReferencingAS
+        # TODO: find_errors for BackReferencingAS-problems
+        warn("`find_errors` for problem with `BackReferencingAS`",
+             " is not implemented yet.")
+        return []
+    end
     super = as(solver, ContinuationSolver)
     errors = find_errors(super)
     # TODO: do some Codim2Solver-specific checks
