b03771f04f8c39350d071ccb91eb9423d6ee023b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-29 19:20:53 -0700

    Test Examples.Bazykin85

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,9 +1,11 @@
 module TestExamples
 include("preamble.jl")
 
+using Bifurcations: Codim1, resolved_points
 using Bifurcations.Continuations: find_errors, print_errors
 using Bifurcations.Examples: PROBLEMS
 using Bifurcations.Examples.Calcium: CalciumParam
+using Bifurcations.Examples.Bazykin85: Bazykin85Param
 
 make_codim2(::Real, point, solver1) = nothing
 make_codim2(::Tuple, point, solver1) = nothing  # PredatorPrey
@@ -16,6 +18,19 @@ make_codim2(::CalciumParam, point, solver1) =
         (0.0, 8.0),
     )
 
+function make_codim2(::Bazykin85Param, point, solver1)
+    if (point.point_type == Codim1.PointTypes.saddle_node ||
+        isapprox(point.u[2], 0; atol=1e-3))
+        return nothing
+    end
+    return BifurcationProblem(
+        point,
+        solver1,
+        (@lens _.δ),
+        (0.0, 10.0),
+    )
+end
+
 @testset "PROBLEMS[$i]" for (i, prob1) in enumerate(PROBLEMS)
     solver1 = init(prob1)
     solve!(solver1)
@@ -23,7 +38,7 @@ make_codim2(::CalciumParam, point, solver1) =
     print_errors(errors)
     @test isempty(errors)
 
-    for point in special_points(solver1)
+    for point in resolved_points(solver1)
         prob2 = make_codim2(prob1.p.de_prob.p, point, solver1)
         if prob2 === nothing
             continue
