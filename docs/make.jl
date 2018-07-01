a813946859f1e1fd98657159049782eb7317425d
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-01 15:56:30 -0700

    Deploy into gh-pages.pre

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,20 @@ Plots.default(dpi=15)
 
 makedocs()
 
-if get(ENV, "TRAVIS", "") == "true"
+# https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
+if get(ENV, "TRAVIS", "") != "true"
+    # Don't do anything outside Travis CI
+elseif startswith(get(ENV, "TRAVIS_BRANCH", ""), "pre/")
+    # For branches pre/*, deploy them into gh-pages.pre.
+    branch = ENV["TRAVIS_BRANCH"]
+    deploydocs(
+        deps   = Deps.pip("mkdocs", "python-markdown-math"),
+        repo   = "github.com/tkf/Bifurcations.jl.git",
+        julia  = "0.6",
+        branch = "gh-pages.pre",
+        latest = branch,
+    )
+else
     deploydocs(
         deps   = Deps.pip("mkdocs", "python-markdown-math"),
         repo   = "github.com/tkf/Bifurcations.jl.git",
