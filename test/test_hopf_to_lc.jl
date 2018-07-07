d863c2c8e5de34a609120f0b68e863cd55629209
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-07 07:40:04 -0700

    Bug: NaN in period should not be ignored silently

--- test/test_hopf_to_lc.jl
+++ test/test_hopf_to_lc.jl
@@ -10,10 +10,10 @@ solve!(solver0)
 
 hopf_point, = special_points(solver0, Codim1.PointTypes.hopf)
 
-period_max = 36.0
+# period_max = 36.0
 prob_lc = LimitCycleProblem(
     hopf_point, solver0;
-    period_bound = (0.0, period_max),
+    # period_bound = (0.0, period_max),
     num_mesh = 20,
     degree = 3,
 )
@@ -33,7 +33,7 @@ plt_multi_panel = plot(
     plot(solver1, (:x=>:parameter, :y=>1, :color=>:period)),
     plot(solver1, (:x=>:parameter, :y=>:period, :color=>:period)),
     plot_state_space(solver1);
-    clims = (10, period_max),  # TODO: make it work without this
+    # clims = (10, period_max),  # TODO: make it work without this
 )
 
 @test_nothrow nullshow(plt_multi_panel)
