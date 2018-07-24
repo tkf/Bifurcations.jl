2b5fb2e5b54029c5c83063468c1b28def537acff
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-23 21:50:23 -0700

    Add some debugging utils in fdiff_utils.jl

--- src/utils/fdiff_utils.jl
+++ src/utils/fdiff_utils.jl
@@ -1,6 +1,8 @@
 module FDiffUtils
 
 using ForwardDiff
+using ForwardDiff: Dual
+using Parameters: @with_kw
 using StaticArrays: SVector
 
 function vderiv2(f, x::Number = 0.0)
@@ -22,6 +24,44 @@ function deriv3(f, x::Number = 0.0)
             b -> ForwardDiff.derivative(f, b),
             a),
         x)
+end
+
+nondual(x::Dual) = nondual(x.value)
+nondual(x::AbstractArray) = nondual.(x)
+nondual(x) = x
+
+_all(f, x::AbstractArray) = all(f, x)
+_all(f, x::Dual) = _all(f, x.value) && all(_all.(Ref(f), x.partials))
+_all(f, x) = f(x)
+_any(f, x) = ! _all(!f, x)
+
+all_isfinite(x) = _all(isfinite, x)
+any_isnan(x) = _any(isnan, x)
+
+@with_kw struct NotFinite <: Exception
+    value
+    expr = nothing
+end
+
+function Base.showerror(io::IO, err::NotFinite)
+    if err.expr != nothing
+        show(io, expr)
+    else
+        print(io, "Value")
+    end
+    println(io, " contains non-finite value(s):")
+    show(IOContext(io, :compact => false), err.value)
+    println(io)
+end
+
+macro assert_finite(expr)
+    quote
+        x = $(esc(expr))
+        if ! all_isfinite(x)
+            error(NotFinite(x, $(QuoteNode(expr))))
+        end
+        x
+    end
 end
 
 end  # module
