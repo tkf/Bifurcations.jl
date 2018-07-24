945e61791e219281ac27f260554a0c8d1dc2915e
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-24 11:09:42 -0700

    Reduce mesh in test_reparametrized_bautin.jl to speedup tests
    Also keep notifying Travis that we are not finished yet.

--- test/test_reparametrized_bautin.jl
+++ test/test_reparametrized_bautin.jl
@@ -1,6 +1,7 @@
 module TestReparametrizedBautin
 include("preamble.jl")
 
+using Compat: @info
 using Setfield: compose
 using StaticArrays: SVector, SMatrix
 
@@ -9,11 +10,14 @@ using Bifurcations.Codim2LimitCycle: FoldLimitCycleProblem
 using Bifurcations.Examples: Bautin
 using Bifurcations.Examples.Reparametrization: orig_p
 
+# Putting @info so that Travis won't stop testing.
 @testset for opts in [
         [],
         [:extra => SVector(-1.0)],
         [:extra => SVector(-1.0), :shift => 0.1, :seed => 1],
         ]
+    @info "TestReparametrizedBautin:"
+    @info "opts = $opts"
 
     prob = reparametrize(
         Bautin.make_prob(),
@@ -26,6 +30,7 @@ using Bifurcations.Examples.Reparametrization: orig_p
     @test codim1_points[1].point_type === Codim1.PointTypes.hopf
     @test codim1_points[1].u[end] ≈ 0  atol=1e-6
 
+    @info "Continuation of Hopf bifurcation..."
     hopf_prob = BifurcationProblem(
         codim1_points[1],
         solver1,
@@ -35,7 +40,7 @@ using Bifurcations.Examples.Reparametrization: orig_p
     hopf_solver = init(
         hopf_prob;
     )
-    solve!(hopf_solver)
+    @time solve!(hopf_solver)
 
     hopf_β₁ = [u[end-1] for sweep in hopf_solver.super.sol.sweeps for u in sweep.u]
     hopf_β₂ = [u[end]   for sweep in hopf_solver.super.sol.sweeps for u in sweep.u]
@@ -43,31 +48,36 @@ using Bifurcations.Examples.Reparametrization: orig_p
     @test maximum(hopf_β₂) > 2
     @test minimum(hopf_β₂) < -2
 
+    @info "Resolving Bautin point..."
     codim2_points = resolved_points(hopf_solver)
     @test length(codim2_points) == 1
     @test codim2_points[1].point_type === Codim2.PointTypes.bautin
     β_bautin = codim2_points[1].u[end-1:end]
     @test all(@. abs(β_bautin) < 1e-6)
 
+    @info "Continuation of fold bifurcation of a limit cycle..."
     flc_prob = FoldLimitCycleProblem(
         codim2_points[1],
         hopf_solver;
-        num_mesh = 20,
+        num_mesh = 10,
         degree = 3,
     )
+    @show flc_prob.super.num_mesh
+    @show flc_prob.super.degree
     @test flc_prob.t_domain == ([-2.0, -2.0], [2.0, 2.0])
     flc_solver = init(
         flc_prob;
         start_from_nearest_root = true,
         max_branches = 0,  # TODO: stop manually doing this
     )
-    solve!(flc_solver)
+    @time solve!(flc_solver)
 
     flc_β₁ = [u[end-1] for sweep in flc_solver.super.sol.sweeps for u in sweep.u]
     flc_β₂ = [u[end]   for sweep in flc_solver.super.sol.sweeps for u in sweep.u]
-    @test all(@. abs(4 * flc_β₁ + flc_β₂^2) < 5e-3)
+    @show maximum(@. abs(4 * flc_β₁ + flc_β₂^2))
+    @test all(@. abs(4 * flc_β₁ + flc_β₂^2) < 5e-2)
     @test maximum(flc_β₂) > 2
-    @test minimum(flc_β₂) > -1e-3
+    @test minimum(flc_β₂) > -1e-2
 end
 
 end  # module
