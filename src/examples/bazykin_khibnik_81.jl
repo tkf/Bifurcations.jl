93c955954c6b401668630a0ac07adecbf5518415
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-25 13:06:44 -0700

    Test BazykinKhibnik81 Jacobian

--- src/examples/bazykin_khibnik_81.jl
+++ src/examples/bazykin_khibnik_81.jl
@@ -7,6 +7,7 @@ using Setfield: @lens
 import Setfield
 
 using ...Bifurcations: BifurcationProblem
+using ...Codim2: DiffEqCodim2Problem
 
 @with_kw struct BazykinKhibnik81Param{N, M, G}
     n::N = 0.5
@@ -35,7 +36,7 @@ make_prob(
         tspan = (0.0, 30.0),
         ode = ODEProblem{!(u0 isa SVector)}(f, u0, tspan, p),
         param_axis = (@lens _.m),
-        t_domain = (0.0, 2.0),
+        t_domain = (-1e-3, 2.0),
         kwargs...) =
     BifurcationProblem(ode, param_axis, t_domain;
                        kwargs...)
@@ -45,5 +46,22 @@ ode = prob.p.de_prob
 u0 = ode.u0
 param_axis = prob.p.param_axis
 t_domain = prob.t_domain
+
+function make_codim2_prob(
+        p = BazykinKhibnik81Param();
+        u0 = SVector(1.0, 0.0),
+        tspan = (0.0, 30.0),
+        ode = ODEProblem{!(u0 isa SVector)}(f, u0, tspan, p),
+        param_axis1 = (@lens _.m),
+        param_axis2 = (@lens _.n),
+        t_domain = ([-1e-3, -1e-3], [2.0, 2.0]),
+        kwargs...)
+    return DiffEqCodim2Problem(
+        ode,
+        param_axis1,
+        param_axis2,
+        t_domain;
+        kwargs...)
+end
 
 end  # module
