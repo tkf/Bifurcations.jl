7e7fc0a4dec1d6737615ee7cb45dc42d025d8dc8
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-28 15:45:52 -0700

    Add profile_morris_lecar.jl

---
+++ misc/profile_morris_lecar.jl
@@ -0,0 +1,8 @@
+include("../benchmark/bench_morris_lecar.jl")
+
+@time flc_solver = pre_solve!(make_morris_lecar_flc_solver())
+@time sweep!(flc_solver)
+
+@time flc_solver = pre_solve!(make_morris_lecar_flc_solver())
+Profile.clear()
+@profile solve!(flc_solver)
