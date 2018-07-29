f77e6112a921bc0dc6528066897429777fea2588
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-29 02:49:37 -0700

    Compare against known points in test_predator_prey.jl

--- test/test_predator_prey.jl
+++ test/test_predator_prey.jl
@@ -1,14 +1,41 @@
 module TestPredatorPrey
 include("preamble_plots.jl")
 
+using Bifurcations: Codim1, resolved_points
 using Bifurcations.Codim2: NormalizingAS, BackReferencingAS
 using Bifurcations.Examples: PredatorPrey
+
+# Bifurcation points calculated in some version of Bifurcations.jl
+CODIM1_KNOWN_POINTS = Dict(
+    # PredatorPrey parameter (tuple) => points
+    (0.0, 3, 5, 3) => [
+        (Codim1.PointTypes.saddle_node,
+         [0.0, 0.0, 0.6]),
+        (Codim1.PointTypes.saddle_node,
+         [1/3, 0, 0.821905296656178]),
+        (Codim1.PointTypes.saddle_node,
+         [0.41116155950979133, 0, 0.8329293250960671]),
+        (Codim1.PointTypes.hopf,
+         [1/3, 0.36576153597718997, 0.6715938474788321]),
+    ],
+)
 
 @testset "smoke PredatorPrey codim-2 ($(nameof(ASType)))" for
         ASType in [NormalizingAS, BackReferencingAS]
 
-    codim1_solver = init(PredatorPrey.prob)
+    codim1_solver = init(PredatorPrey.prob, nominal_angle_rad=0.01)
     solve!(codim1_solver)
+
+    @testset "known points" begin
+        resolved = resolved_points(codim1_solver)
+        known = CODIM1_KNOWN_POINTS[codim1_solver.prob.p.de_prob.p]
+        @test length(resolved) == length(known)
+        for (actual, desired) in zip(resolved, known)
+            type_desired, u_desired = desired
+            @test actual.point_type == type_desired
+            @test actual.u ≈ u_desired  rtol=1e-4
+        end
+    end
 
     for point in special_points(codim1_solver)
         codim2_prob = BifurcationProblem(
