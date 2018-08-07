88675ffb3027f0a2a51c563f29cdf9817036188d
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-07 10:12:39 -0700

    Pre-load modules in docs/build.jl and sandwich warnings

--- docs/build.jl
+++ docs/build.jl
@@ -1,7 +1,23 @@
-using Documenter, Bifurcations
+print_with_color(:blue, let
+    message = " Warnings below (if any) are fine. "
+    margin = (displaysize(STDOUT)[2] - length(message)) ÷ 2
+    ("=" ^ margin) * message * ("=" ^ margin)
+end)
+println()
+flush(STDOUT)
+using Documenter
 using DiffEqBase: AbstractODEProblem
 using Setfield: Lens
 import Plots
+import Jacobi
+import OrdinaryDiffEq
+flush(STDOUT)
+flush(STDERR)
+print_with_color(:blue, "=" ^ displaysize(STDOUT)[2])
+println()
+
+using Bifurcations
+using Bifurcations: plot, plot!  # a workaround
 
 makedocs(
     strict = true,
