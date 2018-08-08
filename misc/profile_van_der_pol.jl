8a1c15ea96665a6bca6e2766e4cff3d436311818
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-07 23:05:38 -0700

    Add profile_van_der_pol.jl

---
+++ misc/profile_van_der_pol.jl
@@ -0,0 +1,8 @@
+include("../benchmark/bench_van_der_pol.jl")
+
+@time lc_solver = pre_solve!(make_van_der_pol_lc_solver())
+@time sweep!(lc_solver)
+
+@time lc_solver = pre_solve!(make_van_der_pol_lc_solver())
+Profile.clear()
+@profile solve!(lc_solver)
